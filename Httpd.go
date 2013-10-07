59ede55c6b93fc1495628b9492499292c6b1f99e
Daisuke Watanabe <daigoro.white@gmail.com>
2013-10-08 00:25:53 +0900

    add function of new page and escape

--- Httpd.go
+++ Httpd.go
@@ -20,19 +20,25 @@ type All struct{
 
 const PagePostFix = ".txt"
 const DataFolder = "wiki/"
+const ViewFolder = "view/"
 
 func viewHandler(w http.ResponseWriter, r *http.Request){
 	title := r.URL.Path[6:]
 	p, _ := loadPage(title)
-	t, _ := template.ParseFiles("view.html")
+	t, _ := template.ParseFiles(ViewFolder + "view.html")
 	t.Execute(w, p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request){
 	title := r.URL.Path[6:]
 	p, _ := loadPage(title)
-	t, _ := template.ParseFiles("edit.html")
+	t, _ := template.ParseFiles(ViewFolder + "edit.html")
 	t.Execute(w, p)
+}
+
+func toEditHandler(w http.ResponseWriter, r *http.Request){
+	title := r.FormValue("title")
+	http.Redirect(w, r, "/edit/" + title , http.StatusFound)
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request){
@@ -45,14 +51,14 @@ func saveHandler(w http.ResponseWriter, r *http.Request){
 
 func allPageHandler(w http.ResponseWriter, r *http.Request){
 	pages, _ := ioutil.ReadDir(DataFolder)	
-	t, _ := template.ParseFiles("all.html")
+	t, _ := template.ParseFiles(ViewFolder + "all.html")
 	html_pages := translateHtml(pages)
 	t.Execute(w, html_pages)
 }
 
 func translateHtml(pages []os.FileInfo) string {
 	html_pages :=  ""
-	t, _ := template.ParseFiles("_wiki_link.html")
+	t, _ := template.ParseFiles(ViewFolder + "_wiki_link.html")
 	for _, page := range pages{
 		buffer := bytes.NewBufferString("")
 		t.Execute(buffer, strings.Replace(page.Name(),".txt","",-1))
@@ -72,13 +78,14 @@ func loadPage(title string) (*Page, error){
 	if err != nil {
 		return &Page{Title: title}, err
 	}
-	return &Page{Title:title, Body:body}, nil
+	return &Page{Title:title, Body: []byte(template.HTMLEscapeString(string(body)))}, nil
 }
 
 func main(){
 	http.HandleFunc("/view/",viewHandler)
 	http.HandleFunc("/edit/",editHandler)
 	http.HandleFunc("/save/",saveHandler)
+	http.HandleFunc("/toedit/",toEditHandler)
 	http.HandleFunc("/",allPageHandler)
 	http.ListenAndServe(":8080",nil)
 }
